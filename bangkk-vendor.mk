760bb5d52e6d32f935693a653c815d36f569b0a0
SGCMarkus <markusornik@gmail.com>
2025-12-17 18:05:48 +0000

    bangkk: Ship MotCamera4

--- bangkk-vendor.mk
+++ bangkk-vendor.mk
@@ -3240,7 +3240,15 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/bangkk/proprietary/product/etc/CarrierSettings/s99999.pb:$(TARGET_COPY_OUT_PRODUCT)/etc/CarrierSettings/s99999.pb
 
 PRODUCT_COPY_FILES += \
+    vendor/motorola/bangkk/proprietary/product/etc/permissions/com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.camera3.xml \
+    vendor/motorola/bangkk/proprietary/product/etc/permissions/com.motorola.moto-uirefresh.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.moto-uirefresh.xml \
+    vendor/motorola/bangkk/proprietary/product/etc/permissions/deviceowner-configuration-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/deviceowner-configuration-com.motorola.camera3.xml \
     vendor/motorola/bangkk/proprietary/product/etc/permissions/privapp-permissions-com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-com.google.android.euicc.xml \
+    vendor/motorola/bangkk/proprietary/product/etc/permissions/privapp-permissions-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-com.motorola.camera3.xml \
+    vendor/motorola/bangkk/proprietary/product/etc/sysconfig/hiddenapi-whitelist-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hiddenapi-whitelist-com.motorola.camera3.xml \
+    vendor/motorola/bangkk/proprietary/system/etc/permissions/com.motorola.motosignature.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.motorola.motosignature.xml \
+    vendor/motorola/bangkk/proprietary/system/etc/permissions/moto-core_services.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/moto-core_services.xml \
+    vendor/motorola/bangkk/proprietary/system/etc/permissions/moto-settings.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/moto-settings.xml \
     vendor/motorola/bangkk/proprietary/system_ext/etc/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
     vendor/motorola/bangkk/proprietary/vendor/etc/acdbdata/Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Bluetooth_cal.acdb \
     vendor/motorola/bangkk/proprietary/vendor/etc/acdbdata/General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/General_cal.acdb \
@@ -3273,6 +3281,10 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/bangkk/proprietary/vendor/etc/init/motorola.hardware.camera.desktop@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/motorola.hardware.camera.desktop@2.0-service.rc \
     vendor/motorola/bangkk/proprietary/vendor/etc/libnfc-sec-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-nci.conf \
     vendor/motorola/bangkk/proprietary/vendor/etc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf \
+    vendor/motorola/bangkk/proprietary/vendor/etc/permissions/com.motorola.camera3.bangkk.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.bangkk.xml \
+    vendor/motorola/bangkk/proprietary/vendor/etc/permissions/com.motorola.camera3.content.ai.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.content.ai.xml \
+    vendor/motorola/bangkk/proprietary/vendor/etc/permissions/com.motorola.camera3.lens.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.lens.xml \
+    vendor/motorola/bangkk/proprietary/vendor/etc/permissions/com.motorola.camera3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.xml \
     vendor/motorola/bangkk/proprietary/vendor/etc/qdcm_calib_data_mipi_moto_vid_csot_1080p_dsc_655.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_mipi_moto_vid_csot_1080p_dsc_655.xml \
     vendor/motorola/bangkk/proprietary/vendor/etc/qdcm_calib_data_mipi_moto_vid_tm_1080p_dsc_655.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_mipi_moto_vid_tm_1080p_dsc_655.xml \
     vendor/motorola/bangkk/proprietary/vendor/etc/qdcm_calib_data_mipi_moto_vid_tm_u8_1080p_dsc_655.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_mipi_moto_vid_tm_u8_1080p_dsc_655.xml \
@@ -3450,8 +3462,10 @@ PRODUCT_PACKAGES += \
     com.qualcomm.qti.mcx.usecase.extension \
     camera.qcom \
     com.qti.chi.override \
+    libAncFaceDetect \
     libBSTSWAD \
     libRbsFlow \
+    libancbase_fd \
     libarcsoft_chi_utils \
     libarcsoft_hdr_detection \
     libarcsoft_high_dynamic_range \
@@ -3479,8 +3493,6 @@ PRODUCT_PACKAGES += \
     libcom.qti.chinodeutils \
     libets_teeclient_v3 \
     libjpege \
-    libancbase_fd \
-    libAncFaceDetect \
     liblowlight_ai_portrait.arcsoft \
     libmctfengine_stub \
     libmmcamera_bestats \
@@ -3509,12 +3521,20 @@ PRODUCT_PACKAGES += \
     sensors.moto \
     sensors.ssc \
     vendor.qti.hardware.camera.postproc@1.0-service-impl \
+    MotoSignatureApp \
     EuiccPartnerApp \
+    MotCamera3AI \
     EuiccGoogle \
     HotwordEnrollmentOKGoogleHEXAGON_WIDEBAND \
     HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
+    MotCamera4 \
+    MotorolaSettingsProvider \
+    com.motorola.motosignature \
+    moto-core_services \
+    moto-settings \
     com.android.hotwordenrollment.common.util \
     capsense_reset \
     android.hardware.biometrics.fingerprint@2.1-service-ets \
-    motorola.hardware.camera.desktop@2.0-service
+    motorola.hardware.camera.desktop@2.0-service \
+    motsettings
 
